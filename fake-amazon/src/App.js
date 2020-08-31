6fe725618cc157968e78c11727cba0e7bc384d92
Nikola Novak <nikola.novak.hr@gmail.com>
2020-08-31 17:32:08 +0200

    Setting up React Router

--- fake-amazon/src/App.js
+++ fake-amazon/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
